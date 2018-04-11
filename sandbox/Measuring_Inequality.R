5e7655c09f283016da1698051b84a4c684316d6b
User1 <user1@User1s-MacBook-Air.local>
2018-04-10 21:16:00 -0700

    All Portland area updates

--- sandbox/Measuring_Inequality.R
+++ sandbox/Measuring_Inequality.R
@@ -40,11 +40,55 @@ Puma_p <- Puma_p%>%
 
 Puma_h <- Puma_h%>%
   select(SERIALNO, ST, PUMA, NP, TYPE, CONP, RNTP, HINCP, WORKSTAT)
+################################################################################
+Puma_allp_p <- Pums_p %>%
+  filter(ST==41)%>%
+  filter(PUMA==1301|
+           PUMA ==1302|
+           PUMA ==1303|
+           PUMA==1305|
+           PUMA ==1314|
+           PUMA ==1316|
+           PUMA ==1317|
+           PUMA ==1318|
+           PUMA ==1319|
+           PUMA ==1320|
+           PUMA ==1321|
+           PUMA ==1322|
+           PUMA ==1323|
+           PUMA ==1324)
+
+#Housing
+Puma_allp_h <- Pums_h %>%
+  filter(ST==41)%>%
+  filter(PUMA==1301|
+           PUMA ==1302|
+           PUMA ==1303|
+           PUMA==1305|
+           PUMA ==1314|
+           PUMA ==1316|
+           PUMA ==1317|
+           PUMA ==1318|
+           PUMA ==1319|
+           PUMA ==1320|
+           PUMA ==1321|
+           PUMA ==1322|
+           PUMA ==1323|
+           PUMA ==1324)
+
+### Choosing variables
+Puma_allp_p <- Puma_allp_p%>%
+  select(SERIALNO, ST, AGEP, PUMA, CIT, CITWP, 
+         NWAB, NWAV, NWLA, NWLK, WAGP, FOD1P,FOD2P, 
+         MIGSP, OCCP, PINCP, POBP, RAC1P, RAC2P, RAC3P)
+
+Puma_allp_h <- Puma_allp_h%>%
+  select(SERIALNO, ST, PUMA, NP, TYPE, CONP, RNTP, HINCP, WORKSTAT)
 
 
 
 ####################################################################################
-Puma_p1 <- Puma_p%>%
+Puma_allp_p1 <- Puma_allp_p%>%
   filter(is.na(WAGP)==FALSE)
 
 #### Function for Gini coefficienct
@@ -53,34 +97,46 @@ ginico<- function(x){
 }
 
 # Gini all people
-Puma_p1 <- Puma_p%>%
+Puma_allp_p1 <- Puma_allp_p%>%
   filter(is.na(WAGP)==FALSE)
-ginico(Puma_p1)
+ginico(Puma_allp_p1)
 
 ### Ginico function by area
 ginico_nbhd<- function(x,y){
-  x <- x%>%
+  f <- x%>%
     filter(is.na(WAGP)==FALSE)%>%
     filter(PUMA==y)
-  print(ineq(x$WAGP, type = "Gini"))
+  print(ineq(f$WAGP, type = "Gini"))
 }
 
 ## N&NE
-ginico_nbhd(Puma_p, '01301')
-
+ginico_nbhd(Puma_allp_p, 1301)
 ## E
-ginico_nbhd(Puma_p, '01302')
-
+ginico_nbhd(Puma_allp_p, 1302)
 ## SE
-ginico_nbhd(Puma_p, '01303')
-
+ginico_nbhd(Puma_allp_p, 1303)
 ## Central East
-ginico_nbhd(Puma_p, '01305')
-
+ginico_nbhd(Puma_allp_p, 1305)
 ## NW&SW
-ginico_nbhd(Puma_p, '01314')
-
-
+ginico_nbhd(Puma_allp_p, 1314)
+## Rest of Multnomah
+ginico_nbhd(Puma_allp_p, 1316)
+## SE Clackamas - Demascus City
+ginico_nbhd(Puma_allp_p, 1317)
+## NW Clackamas - OR City, Milwaukie, Happy Valley
+ginico_nbhd(Puma_allp_p, 1318)
+## NW Clackamas - Lake Oswego, West Linn, Wilsonville
+ginico_nbhd(Puma_allp_p, 1319)
+## SE Washington (county)
+ginico_nbhd(Puma_allp_p, 1320)
+## W Washington (county)
+ginico_nbhd(Puma_allp_p, 1321)
+## Central Washington - Hillboro City
+ginico_nbhd(Puma_allp_p, 1322)
+## Central Washington - Beaverton&Aloha
+ginico_nbhd(Puma_allp_p, 1323)
+## NE Washington - (county)
+ginico_nbhd(Puma_allp_p, 1323)
 
 
 
